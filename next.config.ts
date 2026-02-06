988936353b32250f493137c830441b0dc1d6bf7b
amirimmd <m98amiri@gmail.com>
2026-02-06 11:13:05 +0330

    Sync local project state to main branch

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   
-  // This explicitly prevents Next.js from trying to trace files for middleware on Vercel
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        './.next/server/middleware.js.nft.json' // Explicitly exclude the missing file
-      ],
-    },
-  },
-
   images: {
     remotePatterns: [
       {
